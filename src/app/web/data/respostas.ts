92e28aa7ced68d8d7887337d7b8e7e360db3246c
Ronan Flávio <ronan.flavio@hotmail.com>
2019-12-04 21:24:50 -0300

    chg: trocando dados de json para constante

---
+++ src/app/web/data/respostas.ts
@@ -0,0 +1,24 @@
+import { Resposta } from '../models/resposta';
+
+export const RESPOSTAS: Resposta[] = [
+  {
+    "nome": "Nunca",
+    "valor": 0
+  },
+  {
+    "nome": "Raramente",
+    "valor": 1
+  },
+  {
+    "nome": "Às vezes",
+    "valor": 2
+  },
+  {
+    "nome": "Quase sempre",
+    "valor": 3
+  },
+  {
+    "nome": "Sempre",
+    "valor": 4
+  }
+]
