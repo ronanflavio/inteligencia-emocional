22649061d6d64b488786166611c6626e44252dfe
Ronan Flávio <ronan.flavio@hotmail.com>
2019-12-04 21:24:07 -0300

    new: adicionando imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { InicioComponent } from './web/pages/inicio/inicio.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
+import { InicioComponent } from './web/pages/inicio/inicio.component';
+import { PerguntasComponent } from './web/components/perguntas/perguntas.component';
+import { BaseComponent } from './arch/components/base/base.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    PerguntasComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
