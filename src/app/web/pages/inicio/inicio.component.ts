5a7e1ff3defdfa8f09a0bda76f27b6f30a27259a
Ronan Flávio <ronan.flavio@hotmail.com>
2019-12-04 21:25:56 -0300

    new: incluindo componente na pagina inicial para fins de testes

--- src/app/web/pages/inicio/inicio.component.ts
+++ src/app/web/pages/inicio/inicio.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Pergunta } from '../../models/pergunta';
+import { QuestionarioService } from '../../services/questionario.service';
+import { BaseComponent } from 'src/app/arch/components/base/base.component';
+import { takeUntil, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  perguntas: Pergunta[];
+
+  constructor(
+    private questionarioService: QuestionarioService
+  ) {
+    super();
+  }
 
   ngOnInit() {
+    this.questionarioService.listar()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(res => {
+        this.perguntas = res;
+      });
   }
 
 }
