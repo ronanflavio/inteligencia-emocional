0325e278f480258af56aa951c5d4f9ead5aa2140
Ronan Flávio <c927109@pmmg.mg.gov.br>
2019-12-05 08:16:41 -0300

    new: adicionando módulos necessários para a aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './web/pages/inicio/inicio.component';
@@ -24,9 +27,11 @@ import { BaseComponent } from './arch/components/base/base.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatCardModule,
     MatDividerModule,
+    MatRadioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
