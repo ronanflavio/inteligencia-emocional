841c8617ba96b43c56afabb327809759be3a5fb6
Ronan Flávio <c927109@pmmg.mg.gov.br>
2019-12-06 10:40:25 -0300

    chg: delegando responsabilidades do componente de início new: adicionando descrição do teste

--- src/app/web/pages/inicio/inicio.component.ts
+++ src/app/web/pages/inicio/inicio.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Pergunta } from '../../models/pergunta';
 import { QuestionarioService } from '../../services/questionario.service';
 import { BaseComponent } from 'src/app/arch/components/base/base.component';
 import { takeUntil } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -11,44 +11,24 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class InicioComponent extends BaseComponent implements OnInit {
 
-  passo: number = 1;
-
-  perguntas: Pergunta[];
-
-  respostas: string[];
-
   nome: string;
 
-  resultado: any;
-
   constructor(
-    private questionarioService: QuestionarioService
+    private questionarioService: QuestionarioService,
+    private router: Router
   ) {
     super();
   }
 
   ngOnInit() {
-    this.questionarioService.perguntas()
+    this.questionarioService.obsNome
       .pipe(takeUntil(this.destroy$))
-      .subscribe(res => {
-        this.perguntas = res;
-      });
-
-    this.questionarioService.respostas()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(res => {
-        this.respostas = res;
-      });
+      .subscribe(res => this.nome = res);
   }
 
   salvarNome(event: any) {
-    this.nome = event.nome;
-    this.passo = 2;
-  }
-
-  enviarResultado(event: any) {
-    this.resultado = event;
-    this.passo = 3;
+    this.questionarioService.alterarNome(event.nome);
+    this.router.navigate(['questionario']);
   }
 
 }
