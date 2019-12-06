94d75b4808721dea4a9dc0776a17e5f9d469f068
Ronan Flávio <ronan.flavio@hotmail.com>
2019-12-06 00:05:33 -0300

    new: componente responsável por capturar o nome do usuario

---
+++ src/app/web/components/nome/nome.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-nome',
+  templateUrl: './nome.component.html',
+  styleUrls: ['./nome.component.css']
+})
+export class NomeComponent implements OnInit {
+
+  @Output() salvarNome = new EventEmitter();
+
+  form = this.fb.group({
+    nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+  }
+
+  getErroNome() {
+    let erro = this.form.get('nome').hasError('required') ? 'Este campo é obrigatório' : null;
+    erro = erro ? erro : (this.form.get('nome').hasError('minlength') ? 'Digite pelo menos 3 caracteres.' : null);
+    erro = erro ? erro : (this.form.get('nome').hasError('maxlength') ? 'Digite no máximo 50 caracteres.' : null);
+    return erro;
+  }
+
+  salvar() {
+    this.salvarNome.emit({
+      nome: this.form.get('nome')
+    });
+  }
+
+}
